bfedfabc9022201a5437637683109fc77271ca35
vinhpq <phanvinh0526@gmail.com>
2015-03-21 09:39:25 +0700

    template commit

---
+++ 1112389_1112410/src/App/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package App;
+
+import Frame.FrameDictionary;
+
+/**
+ *
+ * @author vinh
+ */
+public class Main  {
+    
+    public static void main (String[] args){
+        
+       FrameDictionary fd = new FrameDictionary();
+       fd.setVisible(true);
+       
+       
+    }
+}
